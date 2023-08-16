54743690c96c19540dadeb561dee28e5810e1df4
матвей попов <matyha550@gmail.com>
2023-08-16 09:25:50 +0300

    defelopment 0.1.4

--- client/src/assets/components/App/index.tsx
+++ client/src/assets/components/App/index.tsx
@@ -3,10 +3,13 @@ import './css/style.css';
 import { MainContent } from '../MainContent';
 import { ButtonsContainer } from '../ButtonsContainer';
 import { ContextContactData } from '../../hook/contextContactData';
+// import { ContextContactData } from '../../hook/contextContactData';
 
 function App() {
+
+  // const ContextContactData = createContext();
   /* Поля ввода */
-  // const [nameInput, setNameInput] = useState("");                 // Состояние input "ФИО"
+  const [nameInput, setNameInput] = useState("");                 // Состояние input "ФИО"
   //  const [phoneInput, setPhoneInput] = useState("+7");             // Состояние input "Телефон"
   //  const [emailInput, setEmailInput] = useState("");               // Состояние input "E-mail"
   //  const [jobTitleInput, setJobTitleInput] = useState("");         // Состояние input "Должность"
@@ -14,15 +17,17 @@ function App() {
   //  const [nameEventInput, setNameEventInput] = useState("");       // Состояние input "Название мероприятия"
   //  const [responsibleInput, setResponsibleInput] = useState("");   // Состояние input "Ответсвенный"
 
+  // console.log(nameInput);
+
   return (
     <>
 
-      {/* <ContextContactData.Provider value={nameInput}>
-        <ContextContactData.Provider value={setNameInput}> */}
-          <MainContent />
-          <ButtonsContainer />
-        {/* </ContextContactData.Provider>
-      </ContextContactData.Provider> */}
+      <ContextContactData.Provider value={{ nameInput, setNameInput }}>
+        {/* <ContextContactData.Provider value={setNameInput}> */}
+        <MainContent />
+        <ButtonsContainer />
+      </ContextContactData.Provider>
+      {/* </ContextContactData.Provider> */}
     </>
   );
 }
