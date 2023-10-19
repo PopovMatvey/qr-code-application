a7819c708e95453e0610782394b74a0ed4ebf051
матвей попов <matyha550@gmail.com>
2023-10-19 18:29:38 +0300

    bitrix v 2.0.1

--- js/assets/components/CameraContainerWebcam/index.jsx
+++ js/assets/components/CameraContainerWebcam/index.jsx
@@ -1,3 +1,107 @@
+// import React, { useCallback, useRef, useState } from "react";
+// import './css/style.css';
+// import Webcam from "react-webcam";
+// import { HeaderInformation } from "../HeaderInformation";
+
+// // Камера
+// export function CameraContainerWebcam({ title, arrayPhoto }) {
+//     const webcamRef = useRef(null);
+//     const [imgSrc, setImgSrc] = useState(null);
+//     const [captureFlag, setCaptureFlag] = useState(true);
+//     // const [arrayPhoto] = useState([]);
+
+//     // Обработчик "Сделать фото"
+//     const handlerCreateCapture = useCallback(() => {
+//         const imageSrc = webcamRef.current.getScreenshot();
+//         let fileName = `${Math.floor(Math.random() * (1000000000000 - 0 + 1) + 0)}.jpeg`;
+//         console.log(fileName);
+//         setImgSrc({ "fileData": [fileName, imageSrc] });
+
+//         setCaptureFlag(false);
+
+//         console.log(arrayPhoto);
+//     }, [webcamRef, arrayPhoto]);
+
+//     // Обработчик "Сохранить фото"
+//     const handlerSaveCaptuer = () => {
+//         arrayPhoto.push(imgSrc);
+//         setImgSrc(null);
+//         setCaptureFlag(true);
+
+//         console.log("img", imgSrc);
+//         // console.log(arrayPhoto);
+//     }
+
+//     // Обработчик "Удалить фото"
+//     const handlerDeleteCaptuer = () => {
+//         setImgSrc(null);
+//         setCaptureFlag(true);
+
+//         console.log("img", arrayPhoto);
+//     }
+
+//     const FACING_MODE_USER = "user";
+//     const FACING_MODE_ENVIRONMENT = "environment";
+
+//     const videoConstraints = {
+//         facingMode: FACING_MODE_ENVIRONMENT
+//     };
+
+//     // const WebcamCapture = () => {
+//     const [facingMode, setFacingMode] = React.useState(FACING_MODE_ENVIRONMENT);
+
+//     const handleClick = React.useCallback(() => {
+//         setFacingMode(
+//             prevState =>
+//                 prevState === FACING_MODE_USER
+//                     ? FACING_MODE_ENVIRONMENT
+//                     : FACING_MODE_USER
+//         );
+//     }, []);
+//     // }
+
+//     return (
+//         <>
+//             <HeaderInformation title={title} />
+//             <div className="camera-container">
+//                 {
+//                     imgSrc ? (
+//                         <img src={imgSrc.fileData[1]} alt="webcam" />
+//                     ) : (
+//                         <Webcam
+//                             ref={webcamRef}
+//                             screenshotFormat="image/jpeg"
+//                             videoConstraints={{
+//                                 ...videoConstraints,
+//                                 facingMode,
+//                                 frameRate: { ideal: 60, max: 60, },
+//                             }}
+//                         />
+//                     )
+//                 }
+//                 <div className="camera-container_buttons">
+//                     {
+//                         captureFlag ? (
+//                             <>
+//                                 <div className="camera-container_buttons_wrap">
+//                                     <button onClick={handlerCreateCapture}>Сделать фото</button>
+//                                     <button onClick={handleClick}>Сменить камеру</button>
+//                                 </div>
+//                             </>
+//                         ) : (
+//                             <>
+//                                 <div className="camera-container_buttons_wrap">
+//                                     <button onClick={handlerSaveCaptuer}>Сохранить фото</button>
+//                                     <button onClick={handlerDeleteCaptuer}>Удалить фото</button>
+//                                 </div>
+//                             </>
+//                         )
+//                     }
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
 import React, { useCallback, useRef, useState } from "react";
 import './css/style.css';
 import Webcam from "react-webcam";
@@ -6,30 +110,34 @@ import { HeaderInformation } from "../HeaderInformation";
 // Камера
 export function CameraContainerWebcam({ title, arrayPhoto }) {
     const webcamRef = useRef(null);
+    const webcamProcessedRef = useRef(null);
     const [imgSrc, setImgSrc] = useState(null);
     const [captureFlag, setCaptureFlag] = useState(true);
-    // const [arrayPhoto] = useState([]);
 
     // Обработчик "Сделать фото"
     const handlerCreateCapture = useCallback(() => {
-        const imageSrc = webcamRef.current.getScreenshot();
+        // const imageSrc = webcamProcessedRef.current.getScreenshot();
+        let imageSrc = null;
         let fileName = `${Math.floor(Math.random() * (1000000000000 - 0 + 1) + 0)}.jpeg`;
-        console.log(fileName);
+
+        while (imageSrc == null) {
+            imageSrc = webcamRef.current.getScreenshot();
+
+            if (imageSrc != null) {
+                break;
+            }
+        }
+
         setImgSrc({ "fileData": [fileName, imageSrc] });
-
         setCaptureFlag(false);
-
-        console.log(arrayPhoto);
     }, [webcamRef, arrayPhoto]);
+    // }, [webcamProcessedRef, arrayPhoto]);
 
     // Обработчик "Сохранить фото"
     const handlerSaveCaptuer = () => {
         arrayPhoto.push(imgSrc);
         setImgSrc(null);
         setCaptureFlag(true);
-
-        console.log("img", imgSrc);
-        // console.log(arrayPhoto);
     }
 
     // Обработчик "Удалить фото"
@@ -58,26 +166,28 @@ export function CameraContainerWebcam({ title, arrayPhoto }) {
                     : FACING_MODE_USER
         );
     }, []);
-    // }
 
     return (
         <>
             <HeaderInformation title={title} />
             <div className="camera-container">
                 {
-                    imgSrc ? (
-                        <img src={imgSrc.fileData[1]} alt="webcam" />
-                    ) : (
-                        <Webcam
-                            ref={webcamRef}
-                            screenshotFormat="image/jpeg"
-                            videoConstraints={{
-                                ...videoConstraints,
-                                facingMode,
-                                frameRate: { ideal: 60, max: 60, },
-                            }}
-                        />
-                    )
+                    // imgSrc ? (
+                    //     <img src={imgSrc.fileData[1]} alt="webcam" />
+                    // ) : (
+                    <Webcam
+                        ref={webcamRef}
+                        screenshotFormat="image/jpeg"
+                        screenshotQuality={1}
+                        minScreenshotHeight={3000}
+                        minScreenshotWidth={4000}
+                        videoConstraints={{
+                            ...videoConstraints,
+                            facingMode,
+                            frameRate: { ideal: 60, max: 60, },
+                        }}
+                    />
+                    // )
                 }
                 <div className="camera-container_buttons">
                     {
@@ -91,13 +201,25 @@ export function CameraContainerWebcam({ title, arrayPhoto }) {
                         ) : (
                             <>
                                 <div className="camera-container_buttons_wrap">
-                                    <button onClick={handlerSaveCaptuer}>Сохранить фото</button>
-                                    <button onClick={handlerDeleteCaptuer}>Удалить фото</button>
+                                    <button onClick={handlerSaveCaptuer}>Продолжить</button>
+                                    {/* <button onClick={handlerSaveCaptuer}>Сохранить фото</button>
+                                            <button onClick={handlerDeleteCaptuer}>Удалить фото</button> */}
                                 </div>
                             </>
                         )
                     }
                 </div>
+                {/* <div className="webcam-container">
+                    <Webcam
+                        ref={webcamProcessedRef}
+                        screenshotFormat="image/jpeg"
+                        videoConstraints={{
+                            ...videoConstraints,
+                            facingMode,
+                            frameRate: { ideal: 60, max: 60, },
+                        }}
+                    />
+                </div> */}
             </div>
         </>
     )
--- js/assets/page/Camera/index.jsx
+++ js/assets/page/Camera/index.jsx
@@ -7,70 +7,108 @@ import { HeaderInformation } from "../../components/HeaderInformation";
 // import { Html5QrcodeScanner } from 'html5-qrcode';
 import Html5QrcodePlugin from './Html5QrcodeScannerPlugin.jsx'
 import { useState, useCallback } from 'react'
+import { QrScanner } from "@yudiel/react-qr-scanner";
 
 // Камера
 export function Camera(props) {
-    const [scanned, setScanned] = useState(false);
-    const [decodedTextCurrent, setDecodedTextCurrent] = useState("");
-    const onNewScanResult = (decodedText, decodedResult) => {
-        setScanned(true);
-        let tmp = props.decodedText;
-        tmp += " " + decodedText;
-        props.setDecodedText(tmp);
-        setDecodedTextCurrent(decodedText);
+    //     const [scanned, setScanned] = useState(false);
+    //     const [decodedTextCurrent, setDecodedTextCurrent] = useState("");
+    //     const onNewScanResult = (decodedText, decodedResult) => {
+    //         setScanned(true);
+    //         let tmp = props.decodedText;
+    //         tmp += " " + decodedText;
+    //         props.setDecodedText(tmp);
+    //         setDecodedTextCurrent(decodedText);
+    //     };
+
+
+
+    //     // let qrboxFunction = function(viewfinderWidth, viewfinderHeight) {
+    //     //     let minEdgePercentage = 0.7; // 70%
+    //     //     let minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
+    //     //     let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
+    //     //     return {
+    //     //         width: qrboxSize,
+    //     //         height: qrboxSize
+    //     //     };
+    //     // }
+
+    // //     // Create your forceUpdate hook
+    // // function useForceUpdate() {
+    // //     let [value, setState] = useState(true);
+    // //     return () => setState(!value);
+    // // }
+    // //     const handleForceupdateMethod = useForceUpdate();
+
+    //     // const [, updateState] = useState();
+    //     // const handleForceupdateMethod = useCallback(() => updateState({}), []);
+    //     return (
+    //         <>
+    //             <HeaderInformation title={props.title} />
+    //             <div className="camera-container">
+    //                 {/* <h2>Область захвата камеры</h2> */}
+    //                 {/* <React.Fragment> */}
+    //                 {/* <Helmet>
+    //                     <script async={false} src='./html5-qrcode.min.js' />
+    //                     <script async={false} src='./html5-qrcode-demo.js' />
+    //                 </Helmet> */}
+    //                 {/* <div id="qr-reader" style={{width: "400px"}}></div> */}
+    //                 {/* <div id="qr-reader-results" html5QrcodeScanner={docReady}></div> */}
+
+    //                 {/* {(!scanned) && <Html5QrcodePlugin */}
+    //                 {(!scanned) && <Html5QrcodePlugin
+    //                     fps={60}
+    //                     qrbox={props.size}
+    //                     disableFlip={false}
+    //                     qrCodeSuccessCallback={onNewScanResult}
+    //                 />}
+    //                 <br></br><br></br>
+    //             </div>
+    //             {(scanned) && <>
+    //                 <h3>Отсканированная информация: </h3>
+    //                 <h3>{decodedTextCurrent}</h3>
+    //                 <br></br><br></br>
+    //                 <h3>Всё, что было отсканировано: </h3>
+    //                 <h3>{props.decodedText}</h3>
+    //             </>
+    //             }
+    //         </>
+    //     )
+    const [rangeInput, setRangeInput] = useState(100);
+
+    const styleContainerObject = {
+        width: "100%",
+        padding: `${rangeInput}px`,
     };
 
+    const styleVideoObject = {
+        height: "100%",
+    };
 
+    const constraintsObject = {
+        facingMode: 'environment',
+        frameRate: 60,
+        aspectRatio: 1.6,
+        resizeMode: "crop-and-scale",
+    };
 
-    // let qrboxFunction = function(viewfinderWidth, viewfinderHeight) {
-    //     let minEdgePercentage = 0.7; // 70%
-    //     let minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
-    //     let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
-    //     return {
-    //         width: qrboxSize,
-    //         height: qrboxSize
-    //     };
-    // }
+    const handlerRangeinput = (event) => {
+        setRangeInput(event.target.value);
+    };
 
-//     // Create your forceUpdate hook
-// function useForceUpdate() {
-//     let [value, setState] = useState(true);
-//     return () => setState(!value);
-// }
-//     const handleForceupdateMethod = useForceUpdate();
-
-    // const [, updateState] = useState();
-    // const handleForceupdateMethod = useCallback(() => updateState({}), []);
     return (
         <>
             <HeaderInformation title={props.title} />
-            <div className="camera-container">
-                {/* <h2>Область захвата камеры</h2> */}
-                {/* <React.Fragment> */}
-                {/* <Helmet>
-                    <script async={false} src='./html5-qrcode.min.js' />
-                    <script async={false} src='./html5-qrcode-demo.js' />
-                </Helmet> */}
-                {/* <div id="qr-reader" style={{width: "400px"}}></div> */}
-                {/* <div id="qr-reader-results" html5QrcodeScanner={docReady}></div> */}
-
-                {/* {(!scanned) && <Html5QrcodePlugin */}
-                {(!scanned) && <Html5QrcodePlugin
-                    fps={60}
-                    qrbox={props.size}
-                    disableFlip={false}
-                    qrCodeSuccessCallback={onNewScanResult}
-                />}
-                <br></br><br></br>
+            <div className="qr-scanner-container">
+                <QrScanner
+                    onDecode={(result) => {console.log(result); alert(result);}}
+                    onError={(error) => console.log(error?.message)}
+                    containerStyle={styleContainerObject}
+                    videoStyle={styleVideoObject}
+                    constraints={constraintsObject}
+                />
             </div>
-            {(scanned) && <>
-                <h3>Отсканированная информация: </h3>
-                <h3>{decodedTextCurrent}</h3>
-                <br></br><br></br>
-                <h3>Всё, что было отсканировано: </h3>
-                <h3>{props.decodedText}</h3>
-            </>
-            }
+            <input type="range" value={rangeInput} onChange={handlerRangeinput} min={100} max={230} />
         </>
     )
 }
